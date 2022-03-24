a05c407b44a1900d69bc88bb8c3d83499f5d50be
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-03-25 00:02:32 +0600

    2nd-commit

--- src/Components/Client/Client.js
+++ src/Components/Client/Client.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Client.css'
 
 const Client = () => {
+
+    const [clients, setClients] = useState([])
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(res => res.json())
+        .then(data => setClients(data));
+    } ,[])
     return (
         <div className='client-container'>
             <div>
                 <h1>show client</h1>
+                {
+                    clients.map(client => console.log(client))
+                }
             </div>
 
             <div className='calculation'>
---
+++ src/Components/ShowClient/ShowClient.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowClient = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ShowClient;
